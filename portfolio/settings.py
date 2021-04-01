9327dda5de88a7357ed2003f4b7cf25a8ccac554
samweller24 <53188415+samweller24@users.noreply.github.com>
2021-03-31 21:54:28 -0400

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -128,7 +128,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
-#STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
 MEDIA_URL='/media/'
 
